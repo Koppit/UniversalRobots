9576d03980fac2cb7e470bc040d35211edda18cd
Jan Magne Dybvik <jan.m.dybvik@gmail.com>
2026-05-19 10:56:50 +0200

    ryddet i mapper

--- robot/test_robot.py
+++ robot/test_robot.py
@@ -1,27 +1,27 @@
 from transform import transform_robot_coordinates
-from robot.ur3_controller import UR3Controller
+from ur3_controller import UR3Controller
 
 if __name__ == "__main__":
     
     robot = UR3Controller("192.168.0.25")
     robot.connect()
 
-    robot.set_workspace_limits(x=(-0.5, 0.5), y=(-0.5, 0.1), z=(-0.10, 0.55))
+    robot.set_workspace_limits(x=(-1.5, 1.5), y=(-1.5, 1.1), z=(-1.10, 1.55))
 
-    scale = [0.001, 0.001, -0.001]
+    scale = [0.001, -0.001, -0.001]
     translation = [0,0,0]
     rotation = [0,0,0]
 
-
+    # x, y, z , rx, ry, rz
     coords = [[0.0, 0.0, 100.0, 0.0, 0.0, 0.0],
-            [435.0, 285.0, 100.0, 0.0, 0.0, 0.0],
-              [-435.0, 285.0, 100.0, 0.0, 0.0, 0.0]]
+            [434.0, 285.0, 100.0, 0.0, 0.0, 0.0],
+              [-434.0, 285.0, 100.0, 0.0, 0.0, 0.0]]
     transformed_coords = transform_robot_coordinates(coords,scale=scale, translation=translation, rotation=rotation )
 
 
     # Opt-in example — call after connect(), tuned to your physical table setup
-    
-    robot.move_to_xyz_j(transformed_coords[0], safe_z=0.25)
+    for c in transformed_coords:
+        robot.move_to_xyz_j(c, safe_z=0.25)
 
     print(transformed_coords)
 
